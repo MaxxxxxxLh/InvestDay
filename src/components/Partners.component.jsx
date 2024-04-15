24313a8070c64cca6218386f589f2b5401bf277b
SnowZucc <louis.alexandre@eleve.isep.fr>
2024-04-15 19:02:02 +0300

    Add 3vfinance partner

--- src/components/Partners.component.jsx
+++ src/components/Partners.component.jsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 import partnersStyles from "../styles/Partners.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import alttrading from "src/public/assets/partners/alttrading.png";
-import polygon from "src/public/assets/partners/polygon.svg";
-import vantage from "src/public/assets/partners/vantage.png";
+import vfinance from "src/public/assets/partners/3vfinance.svg";
 
 export default function Partners() {
   const router = useRouter();
   return (
     <div className={partnersStyles.container}>
+      <Link href="https://www.3vfinance.com/">
+        <Image src={vfinance} alt="3VFinance" className={partnersStyles.image} />
+      </Link>
     </div>
   );
 }
